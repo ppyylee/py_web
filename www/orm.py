6d2af2eddf4ed09d29a9ce665890dd3bde070810
ppyylee <642188573@qq.com>
2018-04-15 13:12:36 +0800

    finish orm

--- www/orm.py
+++ www/orm.py
@@ -10,60 +10,65 @@ import aiomysql
 def log(sql,args=()):
 	logging.info('SQL: %s' % sql)
 
-async def create_pool(loop,**kw):
-	logging.info('create database connection pool')
-	global __pool
-	__pool = await aiomysql.create_pool(
-		host = kw.get('host','localhost'),
-		port = kw.get('port','3306'),
-		user = kw.get('user'),
-		password = kw.get('pssword'),
-		db = kw.get('db'),
-		charset = kw.get('charset','utf8'),
-		autocommit = kw.get('autocommit',True),
-		maxsize = kw.get('maxsize1',10),
-		minsize = kw.get('minsize',1),
-		loop = loop
-	)
+async def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
+    )
 	
 #select
-async def select(sql,args,size=None):
-	log(sql,args)
-	async with __pool.acquire() as conn:
+async def select(sql, args, size=None):
+    log(sql)
+    global __pool
+    async with __pool.acquire() as conn:
         async with conn.cursor(aiomysql.DictCursor) as cur:
-            await cur.excute(sql.replace('?','%s'),args or ())
-	        if size:
-	        	res = await cur.fetchmany(size)
-	        else:
-	        	res = await cur.fetchall()
-        await cur.close()
-        logging.info('row returned: %s' % len(res))
-        return res
+            await cur.execute(sql.replace('?', '%s'), args or ())
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
 
-#excute insert,update,delete
-async def excute(sql,args):
-	log(sql)
-	async with __pool.acquire() as conn:
-		try: 
-		    async with cur.cursor(aiomysql.DictCursor) as cur:
-			    await cur.excute(sql.replace('?','%s'), args)
-			    rowcount = cur.rowcount
-			await cur.close()
-	    except BaseException as e:
-	    	raise
-	    #返回影响条数
-	    return rowcount 
+#execute insert,update,delete
+async def execute(sql, args, autocommit=True):
+    log(sql)
+    async with __pool.acquire() as conn:
+        if not autocommit:
+            await conn.begin()
+        try:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(sql.replace('?', '%s'), args)
+                affected = cur.rowcount
+            if not autocommit:
+                await conn.commit()
+        except BaseException as e:
+            if not autocommit:
+                await conn.rollback()
+            raise
+        return affected
 
 
-#创建含占位符的字
+#创建含占位符的字符串
 def create_args_string(num):
 	L = []
 	for n in range(num):
 		L.append('?')
-    return ','.join(L)
+	return ', '.join(L)
+
 
 #保存数据列名和类型
-class Field(obj):
+class Field(object):
 
 	def __init__(self,name,column_type,primary_key,default):
 		self.name = name
@@ -71,9 +76,9 @@ class Field(obj):
 		self.primary_key = primary_key
 		self.default = default #默认值
 
-    def __str__(self):
-    	retrun '<%s %s:%s>' % (self.__class__.__name__,self.column_type,self.name)
-
+	def __str__(self):
+		return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+ 
 #具体列名的数据类型
 class StringField(Field):
 
@@ -121,31 +126,31 @@ class ModelMetaclass(type):
 					raise StandardError('Duplicate primary key for field')
 				primary_key = k
 
-            else
+			else:
                 #保存非主键的列名
-                fields.append(k)
-            logging.info('found mapping: %s ==> %s' % (k, v))
-            mapping[k] = v
+				fields.append(k)
+			logging.info('found mapping: %s ==> %s' % (k, v))
+			mapping[k] = v
             #attrs.pop(k)
-        if not primary_key:
-        	raise StandardError('primary key not found')
+		if not primary_key:
+			raise StandardError('primary key not found')
         #清空attrs里的值
-        for k in mapping.keys():
-        	attrs.pop(k)
-        #添加反引号``避免sql关键字冲突报错
-        fields = list(map(lambda f:'`%S`' % f, fields))
-        attrs['__mapping__'] = mapping
-        attrs['__table__'] = tableName
-        attrs['__primary_key__'] = primary_key
-        attrs['__fields'] = fields
-        #SELECT 列名称 FROM 表名称
-        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primary_key, ','.join(fields), tableName)
-        #INSERT INTO table_name (列1, 列2,...) VALUES (值1, 值2,....) 指定插入的列
-        attrs['__insert__'] = 'insert  into `%s` (%s, `%s`) values (%s)' % (tableName, ','.join(fields), primary_key, create_args_string(len(fields)+1))
-        #UPDATE 表名称 SET 列名称 = 新值 WHERE 列名称 = 某值
-        attrs['__update__'] = 'update %s set %s where %s = ? ' % (tableName, ','.join(lambda f: '`%s`=?' % (mapping.get(f).name or f, fields)), primary_key)
-        #DELETE FROM 表名称 WHERE 列名称 = 值
-        attrs['__delete__'] = 'delete from %s where  `%s`=?' % (tableName, primary_key)
+		for k in mapping.keys():
+			attrs.pop(k)
+		#添加反引号``避免sql关键字冲突报错
+		fields = list(map(lambda f:'`%S`' % f, fields))
+		attrs['__mapping__'] = mapping
+		attrs['__table__'] = tableName
+		attrs['__primary_key__'] = primary_key
+		attrs['__fields'] = fields
+		#SELECT 列名称 FROM 表名称
+		attrs['__select__'] = 'select `%s`, %s from `%s`' % (primary_key, ','.join(fields), tableName)
+		#INSERT INTO table_name (列1, 列2,...) VALUES (值1, 值2,....) 指定插入的列
+		attrs['__insert__'] = 'insert  into `%s` (%s, `%s`) values (%s)' % (tableName, ','.join(fields), primary_key, create_args_string(len(fields)+1))
+		#UPDATE 表名称 SET 列名称 = 新值 WHERE 列名称 = 某值
+		attrs['__update__'] = 'update %s set %s where %s = ? ' % (tableName, ','.join(lambda f: '`%s`=?' % (mapping.get(f).name or f, fields)), primary_key)
+		#DELETE FROM 表名称 WHERE 列名称 = 值
+		attrs['__delete__'] = 'delete from %s where  `%s`=?' % (tableName, primary_key)
 		return type.__new__(cls, name, bases, attrs)
 
 #Model基类
@@ -154,9 +159,9 @@ class Model(dict, metaclass=ModelMetaclass):
 	def __init__(self, **kw):
 		super(ClassName, self).__init__(**kw)
 		
-    def __getattr(self, key):
-    	try:
-    		return self[key]
+	def __getattr(self, key):
+		try:
+			return self[key]
 		except KeyError:
 			raise AttributeError(r"'Model' object has no Attribute '%s'" % key)
 
@@ -176,3 +181,66 @@ class Model(dict, metaclass=ModelMetaclass):
 				setattr(self, key, value)
 		return value
 
+    #类方法
+	@classmethod
+	async def findAll(cls, where=None, args=None, **kw):
+		sql = [cls.__select__]
+		if where:
+			sql.append('where')
+			sql.append(where)
+		if args is None:
+			args = []
+		orderBy = kw.get('orderby', None)
+		if orderby is not None:
+			sql.append('orderby')
+			sql.append(orderby)
+		limit = kw.get('limit', None)
+		if limit is not None:
+			sql.append('limit')
+			if isinstance(limit, int):
+				sql.append('?')
+				sql.append(limit)
+			elif isinstance(limit, tuple) and len(limit) == 2:
+				sql.append('?,?')
+				sql.append(limit)
+			else:
+				raise ValueError('Invalid limit value: %s' % str(limit))
+		res = await select(','.join(sql), args)
+		return [cls(**r) for r in res]
+
+	@classmethod
+	async def find(cls, pk):
+		res = await select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+		if len(res) == 0:
+			return  None
+		return cls(**res[0])
+
+	#对象方法
+	async def save(self):
+		args = list(map(self.getValueOrDefault, self.__fields__))
+		args.append(self.getValueOrDefault, self.__primary_key__)
+		res = await execute(self.__insert__, args)
+		if res != 1:
+			logging.warn('failed to insert record: affected rows: %s' % res)
+		return res
+
+	async def update(self):
+		args = list(map(self.getValue, self.__fields__))
+		args.append(self.getValue, self.__primary_key__)
+		res = await execute(self.__update__, args)
+		if res != 1:
+			logging.warn('failed to update record: affected rows: %s' % res)
+		return res
+
+	async def remove(self):
+		args = [self.getValue(self.__primary_key__)]
+		res = await execute(self.__delete__, args)
+		if res!= 1:
+			logging.warn('failed to delete record: affected rows: %s' % res)
+		return res
+
+
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(create_pool(user='root', password='root', db='py_web', loop=loop))
+# res = loop.run_until_complete(select('select * from test', None))
+# print(res)
